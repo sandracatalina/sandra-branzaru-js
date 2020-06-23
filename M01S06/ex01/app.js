58b0ccbd80ed5651626148043337446d8d02f537
sandracatalina <60328895+sandracatalina@users.noreply.github.com>
2020-06-23 04:19:38 +0300

    tema 18 iunie m01s06

--- M01S06/ex01/app.js
+++ M01S06/ex01/app.js
@@ -31,3 +31,36 @@ let car = {
     }, 3000);
   }
 };
+
+//tema
+
+//Afiseaza propozitia: "Masina era marca make si se deplasa cu speed km/h".
+//Decelereaza masina cu 5 unitati apoi afisaza propozitia: "Viteza noua este speed km/h".
+
+console.log(`Masina era marca ${car.make} si se deplasa cu ${car.speed} km/h .`);
+
+for (let i = 0; i <= 4; i++) {
+  car.decelerate();
+}
+
+console.log(`viteza noua este ${car.speed} km/h`);
+
+
+//Adauga o metoda numita stop() care sa faca proprietatea speed 0, apoi afiseaza viteza.
+
+//Adauga o proprietate numita topSpeed si asigneaza-i valoarea 160.
+
+//Adauga o proprietate numita topReverseSpeed si asigneaza-i valoarea -50.
+
+car.stop();
+
+console.log(`viteza este ${car.speed}`);
+
+
+//Adauga  o metoda numita setSpeed() care sa poata primi un parametru fix pentru viteza. Metoda trebuie sa verifice ca nu se depasesc limitele inferioare si superioare, caz in care se folosesc proprietatile topSpeed si topReverseSpeed
+
+car.setSpeed(400);
+console.log(car.speed);
+
+car.setSpeed(-80);
+console.log(car.speed);
--- M01S06/ex02/app.js
+++ M01S06/ex02/app.js
@@ -27,3 +27,38 @@ setTimeout(() => {
 
   // setTimeout(() => {}, 3000);
 }, 3000);
+
+
+//tema
+
+
+// Adauga mesaj pentru pentru Edge.
+//Dupa ce apare elementul h1, asteapta inca 3 secunde si elimina paragraful initial din DOM folosind metoda .remove()
+
+
+if (Array.isArray(userAgentString.match(/Edge/ig))) {
+  message = 'Navighezi folosind Edge';
+  browserName = 'Microsft Edge';
+}
+paragraphElement.innerText = message;
+document.body.appendChild(paragraphElement);
+
+setTimeout( function () {
+  
+  paragraphElement.remove();
+},6000);
+
+
+setTimeout(() => {
+  userName = prompt(`${browserName} vrea sa stie cum te cheama.`);
+  let h1 = document.createElement('h1');
+  h1.innerText = userName;
+  document.body.appendChild(h1);
+
+  // setTimeout(() => {}, 3000);
+
+}, 3000);
+
+
+
+
