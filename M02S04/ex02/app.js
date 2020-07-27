5983ca8f49a49ea48266114a8ed6c7c5400f9334
sandracatalina <60328895+sandracatalina@users.noreply.github.com>
2020-07-27 04:16:56 +0300

    M02S04 tema ex 2

--- M02S04/ex02/app.js
+++ M02S04/ex02/app.js
@@ -1,13 +1,28 @@
-let mainButtons = document.querySelectorAll('.btn');
+et mainButtons = document.querySelectorAll('.btn');
 let box = document.querySelector('.box');
 let colorInput = document.getElementById('color-input');
 let resetButton = document.getElementById('reset-bg');
+let firstBox = document.querySelector('.first-box');
+let secondBox = document.querySelector('.second-box');
+let checkBox = document.querySelector('#my-check');
+let secondCheck = document.querySelector('#second-check');
+
 
 mainButtons.forEach((button) => {
   button.addEventListener('click', () => {
     let colorName = button.innerText.toLowerCase();
-
+    
     box.style.backgroundColor = colorName;
+
+    
+    
+    if (checkBox.checked === true)  {
+      firstBox.style.backgroundColor = colorName;
+    }
+    if (secondCheck.checked === true)  {
+      secondBox.style.backgroundColor = colorName;
+    }
+    
   });
 });
 
@@ -21,8 +36,24 @@ colorInput.addEventListener('change', (event) => {
 
 colorInput.nextElementSibling.addEventListener('click', () => {
   box.style.backgroundColor = colorInput.value;
+
+
+  if (checkBox.checked === true)  {
+    firstBox.style.backgroundColor = colorInput.value;
+  }
+  if (secondCheck.checked === true)  {
+    secondBox.style.backgroundColor = colorInput.value;
+  }
 });
 
 resetButton.addEventListener('click', () => {
+  
   box.removeAttribute('style');
+
+  if (checkBox.checked === true)  {
+    firstBox.removeAttribute('style');
+  }
+  if (secondCheck.checked === true)  {
+    secondBox.removeAttribute('style');
+  }
 });
